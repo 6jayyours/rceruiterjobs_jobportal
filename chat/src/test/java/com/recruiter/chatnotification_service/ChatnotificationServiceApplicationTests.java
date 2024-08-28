497bf8bc7a75bce2e52ba106056d66d8be86253f
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-08-28 16:04:56 +0530

    Added recruiter_chatnotification_service as a subtree in the chat folder

---
+++ chat/src/test/java/com/recruiter/chatnotification_service/ChatnotificationServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.recruiter.chatnotification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatnotificationServiceApplicationTests {
+
+
+}
